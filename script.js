40b0c5b94a5f16b5c03934af607fcbfcfda2a3aa
Anvarkz <140018516+Anvarkz@users.noreply.github.com>
2024-02-13 02:06:09 +0600

    Update script.js

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ $(function () {
     ],
     albumArtworks = ["_1", "_2", "_3", "_4", "_5", "_6", "_7", "_8", "_9", "_10"],
     trackUrl = [
-      "https://youtu.be/YwhkbOMVnMQ?si=wiMf_d34F1vB76Lt",
+      "https://muzmar.net/download/sara-perche-ti-amo-ricchi-e-poveri-testo/YwhkbOMVnMQ",
       "https://eu.hitmotop.com/get/music/20170902/Joe_Dassin_-_Et_si_tu_nexistais_pas_48004460.mp3",
       "https://eu.hitmotop.com/get/music/20110208/Umberto_Tozzi_feat_Monica_Bellucci_-_Ti_amo_391958.mp3",
       "https://eu.hitmotop.com/get/music/20170903/Andrea_Bocelli_-_Cant_Help_Falling_In_Love_48076869.mp3",
